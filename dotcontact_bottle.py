abc8e2725d990679d1de8d75317f9548d07c3468
Arnold Blinn <arnold@arnoldblinn.com>
2020-03-19 10:18:17 -0700

    Added configuration

--- dotcontact_bottle.py
+++ dotcontact_bottle.py
@@ -3,57 +3,19 @@ import json
 from bottle import request, response, HTTPResponse, route, run, ServerAdapter, template, static_file
 from os.path import *
 
-import dns.resolver
-import base64
-
-root = 'dotcontact.me'
-
-def _get_txt(domain):
-        
-    try:
-        dc = '_dotcontact.' + domain
-        answers = dns.resolver.query(qname=dc, rdtype='TXT')
-        if answers and len(answers) == 1:
-            d = b''.join(answers[0].strings)
-            d = base64.b64decode(d)
-            j = json.loads(d)
-            return j
-    except:
-        return None
-
-    return None
-
-def _get_json(host):
-
-    headers = {'Content-Type': 'application/json',  'Access-Control-Allow-Origin' : '*'}
-    if exists('static/' + host + '.json'):
-
-        contents = open('static/' + host + '.json', 'r').read()
-
-        return HTTPResponse(status=200, body=contents, headers=headers)
-    else:
-        contents = _get_txt(host)
-        if contents:
-            return HTTPResponse(status=200, body=json.dumps(contents), headers=headers)
-
-    return HTTPResponse(status=404, body="{}", headers=headers)                          
-
 @route('/', method=['GET', 'OPTIONS'])
 def index():
 
-    return template('query.tpl')
+    return static_file('index.html', root='')
 
-@route('/edit', method=['GET', 'OPTIONS'])
+@route('/edit.html', method=['GET', 'OPTIONS'])
 def edit():
 
-    return template('edit.tpl')
+    return static_file('edit.html', root='')
 
-@route('/dotcontact', method=['GET', 'OPTIONS'])
-def dotcontact():
-
-    domain = request.params['domain']
-
-    return _get_json(domain)
+@route('/configure.html', method=['GET', 'OPTIONS'])
+def configure():
+    return static_file('configure.html', root='')
 
 @route('/ping')
 def ping():
@@ -65,6 +27,7 @@ def ping():
 @route('/static/<filename>')
 def send_static(filename):
     return static_file(filename, root='static')
+
 from gevent import monkey; monkey.patch_all()
 
 if __name__ == '__main__':
